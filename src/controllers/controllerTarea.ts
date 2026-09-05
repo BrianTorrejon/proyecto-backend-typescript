58a78bf94391e49414fa1abaabf901c9ae49f334
briantorrejon <briantorrejon@hotmail.com>
2026-09-04 22:41:52 -0300

    Se crea controlador de tarea

---
+++ src/controllers/controllerTarea.ts
@@ -0,0 +1,13 @@
+import { Tarea } from "../models/tarea.js";
+import { Request, Response } from "express";
+
+const verTarea = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const tarea = await Tarea.find();
+        res.status(200).json(tarea);
+    } catch (error) {
+        res.status(500).json({
+            message: "Error al obtener tarea"
+        });
+    };
+};
